919d9dae916de09759fd4c270981d339c5cda363
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-07-15 11:15:41 -0400

    instanceof

--- src/app.js
+++ src/app.js
@@ -6,3 +6,5 @@ let drone = new Drone(); // creates new instance of the drone
 
 console.log(typeof drone); // object
 console.log(typeof Drone); // function
+console.log(drone instanceof Drone); // returns true
+
