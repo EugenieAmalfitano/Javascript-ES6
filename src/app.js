35776af067fff5ce34667660fcb72e1256852c33
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-07-15 11:22:35 -0400

    Instance properties

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 class Drone {
     constructor(id, name) {
-        console.log('in Drone constructor, id: ' + id + ', name: ' + name);
+        this.id = id;       // instance properties
+        this.name = name;   // cannot be accessed before object is instantiated
     }
 }
 
 let drone = new Drone('A123', 'Flyer'); // creates new instance of the drone
+console.log('id: ' + drone.id + ', name: ' + drone.name);
 
 
 
