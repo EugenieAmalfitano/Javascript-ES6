fb6c4d4f0d6a308ca03f8b80ff478f2cbdfb5a88
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-07-15 14:59:31 -0400

    Inheritance and Code Organization - simple inheritance

--- src/app.js
+++ src/app.js
@@ -1,33 +1,25 @@
-// SUMMARY
-
-/* Classes and Objects
-Insantiating a class, it becomes an bject
-Constuctor - code that automatically runs when you instantiate the object. 
-Properties - pieces of informationthat belong to an instance
-Methods - functions that belong to an instance
-Static Properties and Methods - access these by using class name itself. 
-Getters and Setters - let us execute code when we get or set a property; for example, validate or log. 
- */
+// Inheritance and Code Organization
+// Inheritance - Classes deriving functionality from other classes
+// -- can inherit constructors, properties, and methods. 
 
 const SP = ' ';
-class Drone {
-    constructor(id) {
-        this._id = id;      // underscore usually means private   }
-    }
 
-    // getter can modify or validate the property that is being retrieved
-    get id() {
-        this._id = this._id.toLowerCase();
-        if (this._id.includes('x')) throw 'Cannot contain an X';
-        return this._id;
-    }
+// Vehicle will not be instatiated directly, as with most inherited classes.
+class Vehicle {
 
-    // setter allows assigning a new value to the property
-    set id(value) {
-        this._id = value;
-    }
 }
 
-let drone = new Drone('A123');
-drone.id = 'B456Y';
-console.log('drone id: ' + drone.id);
+class Drone extends Vehicle {
+
+}
+
+class Car extends Vehicle {
+
+}
+
+let c = new Car();
+
+// when we insantiate a class, it's an instance of any inherited class as well as object
+console.log(c instanceof Car)
+console.log(c instanceof Vehicle)
+console.log(c instanceof Object)
