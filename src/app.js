4e7369c5af74c2bac9ed14b8f37d9fcc45e99fd8
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-07-15 11:37:12 -0400

    Static method vs. instance method

--- src/app.js
+++ src/app.js
@@ -4,12 +4,17 @@ class Drone {
         this.id = id;       // instance properties
         this.name = name;   // cannot be accessed before object is instantiated
     }
+    static getCompany() {
+        console.log('in getCompany');
+    }
     fly() {
         console.log('Drone ' + this.id + ' is flying'); // you have to keep 'this' or it won't work.
     }
 }
-Drone.maxHeight = 2000; // Static Property of the Drone class. 
+
 let drone = new Drone('A123', 'Flyer');
 let drone2 = new Drone('B456', 'Twirl');
 drone.fly();
 drone2.fly();
+Drone.getCompany(); // static method only works on the class
+drone.getCompany(); // static method does not work on the instance.
