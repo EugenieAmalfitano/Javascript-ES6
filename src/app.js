c7591b8a6ca635f621a76d322748d04490145835
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-07-15 11:29:04 -0400

    Static property of a class

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
+const SP = ' ';
 class Drone {
     constructor(id, name) {
         this.id = id;       // instance properties
         this.name = name;   // cannot be accessed before object is instantiated
     }
 }
+Drone.maxHeight = 2000; // Static Property of the Drone class. 
 
 let drone = new Drone('A123', 'Flyer'); // creates new instance of the drone
 console.log('id: ' + drone['id'] + ', name: ' + drone['name']);
+
+let drone2 = new Drone('B456', 'Twirl');
+console.log(drone.id + SP + drone2.id + SP + drone.maxHeight) // maxHeight is undefined because it's a static property of the class, not the instance.
 
 
 
