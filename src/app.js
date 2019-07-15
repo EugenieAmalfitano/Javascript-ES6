2b8893bb9a4937761b04f61c130e90229aed492c
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-07-15 10:58:02 -0400

    Completed setup. App.js talks about strict mode. Not sure this is working as discussed.

---
+++ src/app.js
@@ -0,0 +1,7 @@
+droneId = 5;
+console.log('droneID ' + droneId);
+console.log('droneID ' + window.droneId);
+
+let droneId2 = 5;
+console.log('droneID ' + droneId2);
+console.log('droneID ' + window.droneId2); // returns undefined; let scopes to this. 
