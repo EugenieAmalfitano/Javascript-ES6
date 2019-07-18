8caaf9948014771e7fe674495aba4581e3baa310
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-07-18 12:58:55 -0400

    Added a drone image and image.js UI class. Modified launch.json to point to the startup app.js for eventual debug.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { Car } from './classes/car.js';
 import { Drone } from './classes/drone.js';
 import { fleet } from './fleet-data.js';
 import { FleetDataService } from '../services/fleet-data-service.js'
+import { Image } from '../UI/image.js';
 //For some reason, the following line is not needed to access jquery because I have TypeScript installed
 //import $ from 'jquery'; 
 
@@ -10,6 +11,9 @@ import { Button } from '../UI/button.js';
 
 let b = new Button('Click me');
 b.appendToElement($('body'));
+
+let i = new Image('./UI/images/drone.jpg');
+i.appendToElement($('body'));
 
 const NewLine = '\r\n';
 let dataService = new FleetDataService();
